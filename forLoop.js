b6b651bad54ebb3e09d5a4d2d9d747b09128aa9f
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-05 18:10:06 +0600

    Add files via upload

---
+++ forLoop.js
@@ -0,0 +1,8 @@
+for(var sum = 0; sum < 20; sum++ ){
+console.log(sum)
+}
+
+var number = [0,1,2,3,4,5,6,7,8,9,"Rakib","Akib","Siam"]
+for (var i = 0; i<number.length; i++){
+    console.log(number[i])
+}
---
+++ whileLoop.js
@@ -0,0 +1,11 @@
+var i = 0;
+while(i <= 10){
+    console.log(i);
+    i++
+}
+
+var n = 1;
+while(n <= 20){
+    console.log(n)
+    n++
+}
